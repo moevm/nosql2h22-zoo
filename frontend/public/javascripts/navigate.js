77401c6d3f8c0df7b6a038c9f066dc342587ad4e
Карпекина Анна <44322680+Anny45@users.noreply.github.com>
2022-11-27 17:51:46 +0300

    Add files via upload

---
+++ frontend/public/javascripts/navigate.js
@@ -0,0 +1,30 @@
+function toMain(){
+    document.location = 'https://localhost:3000';
+}
+
+function toAnimals(){
+    document.location = 'https://localhost:3000/animals.html';
+}
+function toInfo(){
+    document.location = 'https://localhost:3000/about_the_zoo.html';
+}
+
+function toMap(){
+    document.location = 'https://localhost:3000/map.html';
+}
+
+function toShedule(){
+    document.location = 'https://localhost:3000/schedule.html';
+}
+
+function toBuy(){
+    document.location = 'https://localhost:3000/buy_tickets.html';
+}
+
+function toSponsor(){
+    document.location = 'https://localhost:3000/sponsors.html';
+}
+
+function toEmployees(){
+    document.location = 'https://localhost:3000/employees.html';
+}
