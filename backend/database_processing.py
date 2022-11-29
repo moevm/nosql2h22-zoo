444e38213c7f6ed6d7e7ff0e1ba6f6a07a875807
Muirehe <54913485+SonKhwa@users.noreply.github.com>
2022-11-29 21:40:35 +0300

    added EntityTypes to database_processing

--- backend/database_processing.py
+++ backend/database_processing.py
@@ -1,5 +1,5 @@
 from pymemcache.client import base
-from enum import Enum
+from db_types import EntityTypes, EmployeeTypes
 import json
 import os
 
@@ -44,15 +44,6 @@ def set_value(key, value):
 def set_many(json_data):
     return client.set_many(json_data)
 
-
-class Types(Enum):
-    ticket = 0
-    timetable = 1
-    employee = 2
-    animal = 3
-    schedule = 4
-
-
 def import_database_to_file(data):
     with open(os.path.dirname(os.path.abspath(__file__)) + "\\database.json", "w", encoding='utf-8') as f:
         f.write(json.dumps(data, ensure_ascii=False))
@@ -65,7 +56,7 @@ def export_database_from_file():
 
 
 def get_database():
-    return get_many([key.name for key in Types])
+    return get_many([key.name for key in EntityTypes])
 
 
 def get_collection(key):
@@ -82,7 +73,7 @@ def add_to_database(data):
         data = json.loads(data.encode('utf-8'))
 
     items = {}
-    for key in Types:
+    for key in EntityTypes:
         collection = get_value(key.name)
         if key.name in data:
             if collection == None:
