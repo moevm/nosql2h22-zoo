dc7b9de572b6c13319d0341d7357e8f75236e3af
Карпекина Анна <44322680+Anny45@users.noreply.github.com>
2022-12-03 01:24:07 +0300

    Update TimetableCreate.jsx

--- frontend/src/modals/TimetableCreate/TimetableCreate.jsx
+++ frontend/src/modals/TimetableCreate/TimetableCreate.jsx
@@ -41,14 +41,16 @@ export const TimetableCreate = () => {
                             <Grid container spacing={2} mt={2}>
                                 <Field name="day">
                                     {props => (
-                                        <Grid item xs={12}>
-                                            <TextField
-                                                fullWidth
-                                                label="Day"
-                                                name={props.input.name}
-                                                value={props.input.value}
-                                                onChange={props.input.onChange}
-                                            />
+                                        <Grid item xs={12} >
+                                            <select name={props.input.name} value={props.input.value} onChange={props.input.onChange}>
+                                                <option value="Monday">Monday</option>
+                                                <option value="Tuesday">Tuesday</option>
+                                                <option value="Wednesday">Wednesday</option>
+                                                <option value="Thursday">Thursday</option>
+                                                <option value="Friday">Friday</option>
+                                                <option value="Saturday">Saturday</option>
+                                                <option value="Sunday">Sunday</option>
+                                            </select>
                                         </Grid>
                                     )}
                                 </Field>
